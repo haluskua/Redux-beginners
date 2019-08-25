fe7f2cf5d378decf8fbcc50ea22aa84fee7d4677
haluskua <haluskua.one@gmail.com>
2019-08-25 17:57:46 +1200

    redux with react

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from "react";
-// import logo from "./logo.svg";
-// import "./App.css";
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement } from "./actions";
 
 function App() {
+  const counter = useSelector(state => state.counter);
+  const isLogged = useSelector(state => state.isLogged);
+  const dispatch = useDispatch();
+
   return (
     <div className="App">
-      <h1>Hello world!</h1>
+      <h1>Counter {counter}</h1>
+      <button onClick={() => dispatch(increment(5))}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
+      <div className="log">
+        {isLogged ? <h3>Valuable information I should see</h3> : ""}
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,48 +3,19 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-
 import { createStore } from "redux";
+import allReducer from "./reducers";
+import { Provider } from 'react-redux'
 
-// STORE -> GLOBALIZED STATE
+const appStore = createStore(
+  allReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
-// ACTION -->describes what you want to do basically describes the action
-//         all an action is, is just a simple function that returns an objec
+ReactDOM.render(
+<Provider store={appStore}>
+<App />
+</Provider>,
+ document.getElementById("root"));
 
-const increment = () => {
-  return {
-    type: "INCREMENT"
-  };
-};
-const decrement = () => {
-  return {
-    type: "DECREMENT"
-  };
-};
-
-//REDUCER --> describes how your actions transform the state into another stoate - reducer checks action and based on the action it will modifies the STORE
-const counter = (state = 0, action) => {
-  switch (action.type) {
-    case "INCREMENT":
-      return state + 1;
-
-    case "DECREMENT":
-      return state - 1;
-    default:
-  }
-};
-
-let store = createStore(counter);
-
-//display it in the console
-store.subscribe(() => console.log(store.getState()));
-//Dispatch --> this is the way where we can execute the actions == dispatch your action to the reducer which checks "what to do and updates the STORE"
-store.dispatch(increment());
-store.dispatch(decrement());
-
-ReactDOM.render(<App />, document.getElementById("root"));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
